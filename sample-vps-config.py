93f6a6029101378bb9c17bff4a29c5fcf56d0dba
SNnT23 <79445089+SNnT23@users.noreply.github.com>
2023-08-18 17:24:03 +0630

    Update sample-vps-config.py

--- sample-vps-config.py
+++ sample-vps-config.py
@@ -10,11 +10,11 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class Config(object):
 
-    TG_BOT_TOKEN = ""
-    APP_ID = 
-    API_HASH = ""
+    TG_BOT_TOKEN = "5433367337:AAEGFJqGKDO9Vm9LClMToyLRmgGXaDwiU30"
+    APP_ID = "11760324"
+    API_HASH = "28c6f268d78f2890fd75d614c36a3136"
     TG_USER_SESSION = ""
-    DB_URI = ""
+    DB_URI = "mongodb+srv://admin123:clone123@cluster0.tr05beb.mongodb.net/?retryWrites=true&w=majority"
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
