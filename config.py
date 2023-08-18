6df3a94ff0beff663dcb095b1e8dbcb936c8dcea
SNnT23 <79445089+SNnT23@users.noreply.github.com>
2023-08-18 17:54:55 +0630

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config(object):
     APP_ID = "11760324"
     API_HASH = "28c6f268d78f2890fd75d614c36a3136"
     TG_USER_SESSION = ""
-    DB_URI = "mongodb+srv://admin123:clone123@cluster0.tr05beb.mongodb.net/?retryWrites=true&w=majority"
+    DB_URI = "mongodb+srv://admin123:clone098@cluster0.tr05beb.mongodb.net/?retryWrites=true&w=majority"
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
